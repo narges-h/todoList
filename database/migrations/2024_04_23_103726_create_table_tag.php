994fd700f65bada9f884e1787a5d283bacd21421
narges-h <narges.80.hoseyni@gmail.com>
2024-04-24 20:01:03 +0330

    add migration

--- database/migrations/2024_04_23_103726_create_table_tag.php
+++ database/migrations/2024_04_23_103726_create_table_tag.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('infos', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('tag');
         });
     }
 
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('infos');
+        Schema::dropIfExists('tags');
     }
 };
